88e2b0448043a51b3ecb9a97b3a2a050ab328df8
Liss Heidrich <user@server.fake>
2021-12-07 09:21:15 +0100

    added readme

--- src/lib.rs
+++ src/lib.rs
@@ -31,49 +31,8 @@ macro_rules! json_nav_internal {
     };
 }
 
-/// A macro for conveniently navigating `serde_json::Values` without having
-/// to do all the error handling manually. This is especially useful
-/// for situations where you have do not have consistent or predictable json documents
-/// and you want to try multiple paths to find the one where the relevant information
-/// is located.
-///
-/// # Examples
-///
-/// ```rust
-/// use serde_json::{json, Value};
-/// use json_nav::{json_nav, JsonNavError};
-///
-/// let value = json!({
-///     "code": 200u16,
-///     "success": true,
-///     "payload": {
-///         "features": [
-///             "serde",
-///             "json"
-///         ]
-///     }
-/// });
-///
-/// let first_feature = json_nav! {
-///     value => "payload" => "features" => 0; as str
-/// };
-///
-/// assert_eq!(Ok("serde"), first_feature);
-///
-///
-/// let type_error = json_nav! {
-///     value => "payload" => "features" => 1; as object
-/// };
-///
-/// assert_eq!(Err(JsonNavError::TypeMismatch { expected: "object" }), type_error);
-///
-///
-/// let path_error = json_nav! {
-///     value => "payload" => "failure"
-/// };
-///
-/// assert_eq!(Err(JsonNavError::Navigation { path: "value.payload.failure" }), path_error);
-/// ```
+
+#[doc = include_str!("../README.md")]
 #[macro_export]
 macro_rules! json_nav {
     ($json:expr => $($path:expr)=>+) => {
